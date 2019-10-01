2b5ed7c11a8ed1ae96d8fa2015b934fc49f6bfb0
AlviAhmed <alviahmed1234@gmail.com>
2019-10-01 12:29:18 -0400

    updated emacs config

---
+++ .emacs.d/elpa/treemacs-icons-dired-20190327.1558/treemacs-icons-dired-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-icons-dired" "20190327.1558" "Treemacs icons for dired" '((treemacs "0.0") (emacs "25.2") (cl-lib "0.5")) :commit "32a241d0aaa5c713815a1a040eea52a5c2b01589" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
